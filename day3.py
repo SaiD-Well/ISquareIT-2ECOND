6b45e2cd948a90082956f726c5f7c8ae33938ff6
SaiD-Well <said_c23173@students.isquareit.edu.in>
2025-02-07 10:48:39 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.title("Welcome Everyone, We are learning PYTHON")
+st.write("Python requires practice!!!")
+
+data = pd.DataFrame({'c1':[10,20,30,40],'c2':['A','B','C','D']})
+
+st.write("Below is table for Data")
+
+st.write(data)
+chart_data = pd.DataFrame(np.random.randn(20,3),columns=['A','B','C'])
+st.line_chart(chart_data)
+st.bar_chart(chart_data)
+st.area_chart(chart_data)
